6079b1b3bc40c164d0e6cde2ba23248b509e4071
Tom Lambert <tom.lambert@one-agency.be>
2016-06-10 18:12:20 +0200

    Refactor Buildserver class

--- lib/buildserver.rb
+++ lib/buildserver.rb
@@ -1,140 +1,135 @@
 require 'jenkins_api_client'
 require 'colorize'
+require 'titleize'
 
+##
+# This class represents a Jenkins server.
 class BuildServer
 
-	@environments 	= Array.new
-	
-
-	def initialize (username, password, server_ip, project_name, namespace)
-		########## constructor ################################################################################
-		#
-		# @string: username, @string: password, @string: server_ip, @string: project_name, @string: namespace
-		# We'll initialize the class here.
-		# We'll also generate the object that we need for the communication with jenkins.
-		#
-		######################################################################################################
-		@USERNAME  		= username
-		@PASSWORD  		= password
-		@SERVER_IP 		= server_ip
-		@PROJECT_NAME 	= project_name
-		@NAMESPACE		= namespace
+  ##
+  # Creates a new buildserver and generate a JenkinsApi::Client object.
+  #
+  # ==== Attributes
+  #
+  # * +username+ - The username to connect to the Jenkins server.
+  # * +password+ - The password or API token for the user.
+  # * +server+ - The host name or IP address to connect to.
+  # * +project_name+ - The name you want to give to your project.
+	def initialize(username, password, server, project_name)
+		@username  		= username
+		@password  		= password
+		@server   		= server
+		@project_name = project_name
 
 		@client = JenkinsApi::Client.new(
-
-										 :server_ip 	=>	@SERVER_IP,
-										 :username 		=>	@USERNAME,
-										 :password 		=>	@PASSWORD,
-										 :log_location 	=>	'/var/lib/jenkins/projectify/' + project_name + '_jenkins.log'
-										
-										)
-
+                  :server_ip 	=>	@server,
+                  :username 		=>	@username,
+                  :password 		=>	@password,
+                  :log_location 	=>	'/var/lib/jenkins/projectify/' + project_name + '_jenkins.log'
+                )
 	end
 
-	def set_environment (environments)
-		########## set_environment ####################################################################
-		#
-		# @array of strings: environments
-		# We'll set the environments that we have to make build jobs for.
-		# In addition, we will also generate the job names that we can use later on for our API calls.
-		#
-		###############################################################################################
+  ##
+  # Setter for @environments and generate the Jenkins job names.
+  #
+  # ==== Attributes
+  #
+  # * +environments - An array of strings.
+	def set_environments(environments)
+    @environments = []
 		@environments = environments
-		generate_job_names()
+
+		generate_job_names
 	end
 
-	def generate_job_names()
-		########## generate_job_names ########################################################
-		#
-		# We'll generate the job names.
-		# We'll also add the environment type with a pipe to the name for future use.
-		#
-		######################################################################################
-		@job_names 	= Array.new
-		@environments.each do |build_env|
-			if build_env == "master"
-				build_dev_env = "dev"
+  ##
+  # Generate the job names for the Jenkins jobs.
+	def generate_job_names
+		@job_names 	= []
+
+		@environments.each do |environment|
+			if environment == "master"
+				env = "dev"
 			else
-				build_dev_env = build_env
+				env = environment
 			end
-			@job_names.push("(#{@PROJECT_NAME.upcase}-#{build_dev_env}) deploy|#{build_dev_env}")
-			@job_names.push("(#{@PROJECT_NAME.upcase}-#{build_dev_env}) deploy:update|#{build_dev_env}")
+
+      name = @project_name.titleize.tr('-', '')
+
+      @job_names.push("#{name}-#{env.upcase}-deploy")
+      @job_names.push("#{name}-#{env.upcase}-deployUpdate")
 		end
 	end
 
-	def set_notifier(notifier_email)
-		########## set_notifier ########################################################
-		#
-		# @string: notifier_email
-		# We'll set the email that will be used for notification emails.
-		#
-		################################################################################
-		@notifier = notifier_email
-	end
-
+  ##
+  # Generate a success message
+  #
+  # ==== Attributes
+  #
+  # * +message+ - The message to display.
 	def good_job(message)
-		########## good_job ########################################################
-		#
-		# @string: message
-		# Function for generating a success message
-		#
-		############################################################################
-		puts "{" + "+".green + "} Succesfully created the following job: #{message}"
+    puts "{#{'+'.green}} Succesfully created the following job: #{message}"
 	end
 
+  ##
+  # Generate a error message
+  #
+  # ==== Attributes
+  #
+  # * +message+ - The message to display.
 	def bad_job(message)
-		########## bad_job ########################################################
-		#
-		# @string: message
-		# Function for generating an error message
-		#
-		###########################################################################
-		puts "{" + "-".red + "} Failed to create the following job: #{message}"
+    puts "{#{'+'.red}} Failed to created the following job: #{message}"
 	end
-	def add_build_jobs()
-		########## add_build_jobs ############################################################################################
-		#
-		# We will loop through all job names and do the following actions:
-		# - First we'll see what type of jobs we have and set the type_string (type: string)
-		# - We'll also see what environments we have, and if we encounter dev, we'll set the branch to master (type: string)
-		# - When all that is set, we will start creating the job through the jenkins api object (@client).
-		# - The jenkins api object (@client) will return a status code. If it's 200, we know the job has been created. 
-		#
-		#####################################################################################################################
+
+  ##
+  # Add the build jobs to the Jenkins server
+	def add_build_jobs
 		@job_names.each do |job|
 
-			if job.split('|')[0].include?("update")
-				type_string = " update"
+			if job.include?("deployUpdate")
+				task = ":update"
 			else
-				type_string = ""
+				task = ""
 			end
 
-			if job.split('|')[1].include?("dev")
-				build_dev_env = "master"
-			else
-				build_dev_env = job.split('|')[1]
+			if job.include?("-DEV-")
+				branch = "master"
+				env = "dev"
+      elsif job.include?("-TEST-")
+				branch = "test"
+				env = "testing"
+      elsif job.include?("-ACC-")
+				branch = "acc"
+				env = "acc"
+      elsif job.include?("-STAG-")
+				branch = "staging"
+				env = "staging"
+      elsif job.include?("-PROD-")
+				branch = "prod"
+				env = "projd"
 			end
+
 			begin 
 				deploy = @client.job.create_or_update_freestyle(
-																:name 				=>	"#{job.split('|')[0]}",
-																:keep_dependencies	=>	true,
-																:scm_provider		=>	'git',
-																:scm_url			=>	"git@gitlab.crosscheck.be:#{@NAMESPACE}/#{@PROJECT_NAME}",
-																:scm_branch			=>	build_dev_env,
-																:shell_command		=>	"cap #{job.split('|')[1]} deploy#{type_string}",
-																:notification_email	=>	@notifier
-																)
+                      :name 	      			=>	job,
+                      :keep_dependencies	=>	true,
+                      :scm_provider		    =>	'git',
+                      :scm_url			      =>	"git@bitbucket.org:ausybenelux/#{@project_name}",
+                      :scm_branch	    		=>	branch,
+                      :shell_command		=>	"cap #{env} deploy#{type_string}",
+                      )
+
 				if deploy == "200"
-					good_job("#{job.split('|')[0]}")
+					good_job("#{job}")
 				else
-					bad_job("#{job.split('|')[0]}")
+					bad_job("#{job}")
 				end
+
 			rescue 
 				@error_message	= "#{$!}"
+
 				bad_job("#{@error_message}")
 			end
-
-
 		end
 	end
 end
